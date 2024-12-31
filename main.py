0886ebe4fb01ae9cbfcc642d59b481ffa2dad2ba
Aminkay95 <149305078+Aminkay95@users.noreply.github.com>
2024-12-31 12:39:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,21 @@ from email.mime.multipart import MIMEMultipart
 # This function gets current price for the coins from the coinmarketcap api and returns a key value pair of the name of the coin and its price
 
 def crypto_rates(base_currency = 'USD', assests = 'bitcoin,ethereum,xrp' ):
+    """
+    Fetches the latest prices of the specified cryptocurrencies in the base currency
+
+    Parameters:
+    -----------
+    base_currency: str 
+        This is the fiat currency in which the prices of the cryptocurrencies will be converted to
+    assests: str
+        This a comma-seperated string of the desired cryptocurrencies slugs
+    
+    Returns:
+    --------
+    crypto_dict
+        A dictionary where the keys are names of the crypto and the values are their corresponding prices
+    """
     url = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest'
     parameters = {
     'slug': assests,
@@ -37,7 +52,19 @@ def crypto_rates(base_currency = 'USD', assests = 'bitcoin,ethereum,xrp' ):
 
 # Function to send email alert 
 def setAlert(subject, body, to_email):
+    """
+    Sends an alert through email. The function uses gmail smtp server.
 
+    Parameters:
+    -----------
+    subject: str
+        subject line of the email
+    body: str
+        The main of the email
+    to_email: str
+        The recipient's email addrress
+    
+    """
      
     smtp_server = "smtp.gmail.com"
     smtp_port = 587
@@ -65,7 +92,7 @@ def setAlert(subject, body, to_email):
     except Exception as e:
         print(f"Failed to send email: {e}")
 
-
+# Using loop to enter infinite loop in order this script runs perpetually. if hosted in the cloud no need to enter the loop. 
 while True:
 
     data = crypto_rates()
@@ -79,8 +106,10 @@ while True:
         message += f"The Crypto Currency: {crypto_currency[i]} is valued at {crypto_value[i]} USD\n"
 
     setAlert("CRYPTO PRICE ALERT", message, "mohamedamino0316@gmail.com")
-
-    sleep(86400)
+    
+    # Sleep for a day to ensure this script is run only once per day. However if hosted in the cloud this delay is not neccessary.
+    
+    sleep(24*60*60)
 
     
     
